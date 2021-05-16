9bba7f13934c5506ce8d6cc0dbaa0e3f128bd045
だい <dai.03.chi.18@gmail.com>
2021-05-17 03:15:39 +0900

    ハンバーガーメニュー実装

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,13 +1,20 @@
 $(function () {
-  var slideCurrent = 0;
-  var lastCurrent = $('.el_slide_list').length - 1;
+  // ハンバーガーメニュー
+  $('.js_btn , .js_nav').on('click', function () {
+    $('.bl_nav , .el_btn_line , .el_nav_item > a').toggleClass('is_open');
+  })
 
-  $('.el_slide_list').css('display', 'none'); // 一旦すべてのスライドを非表示
-  $('.el_slide_list').eq(slideCurrent).css('display', 'block'); // 最初のスライドを表示
+
+  // スライドショー
+  var slideCurrent = 0;
+  var lastCurrent = $('.js_slide_list').length - 1;
+
+  $('.js_slide_list').css('display', 'none'); // 一旦すべてのスライドを非表示
+  $('.js_slide_list').eq(slideCurrent).css('display', 'block'); // 最初のスライドを表示
 
   function changeslide() {
-    $('.el_slide_list').fadeOut(1500);
-    $('.el_slide_list').eq(slideCurrent).fadeIn(1500);
+    $('.js_slide_list').fadeOut(1500);
+    $('.js_slide_list').eq(slideCurrent).fadeIn(1500);
   };
 
   var Timer;
@@ -22,7 +29,5 @@ $(function () {
       };
     }, 3000);
   }
-
   startTimer();
-
 });
